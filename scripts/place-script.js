3819447e3737a61d98447c1163a7d5266018f6fb
FrancoBrunini <valentinobrunini@gmail.com>
2026-03-28 19:06:44 -0300

    c

--- scripts/place-script.js
+++ scripts/place-script.js
@@ -3,3 +3,13 @@ year.textContent = new Date().getFullYear();
 
 const lastModified = document.querySelector("#lastModified");
 lastModified.textContent = `Last Modification: ${document.lastModified}`;
+const temp = 8;   
+const wind = 10;  
+function calculateWindChill(t, w) {
+    return (13.12 + 0.6215 * t - 11.37 * Math.pow(w, 0.16) + 0.3965 * t * Math.pow(w, 0.16)).toFixed(1);
+}
+let windChill = "N/A";
+if (temp <= 10 && wind > 4.8) {
+    windChill = calculateWindChill(temp, wind);
+}
+document.getElementById("windChill").textContent = windChill;
